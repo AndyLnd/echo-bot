16e58bdd4c87bfe02c0f6b2cd2cfb97d682a1340
AndyTheGiant <andy.bialystock@gmail.com>
2018-02-13 14:22:58 +0100

    debug

--- src/main/java/com/wire/bots/scrabbler/WordList.java
+++ src/main/java/com/wire/bots/scrabbler/WordList.java
@@ -1,27 +1,29 @@
 package com.wire.bots.scrabbler;
 
 import com.wire.bots.sdk.Logger;
+
+import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WordList {
   private Set<String> wordList;
 
   WordList() {
-    try {
-      ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-      File file = new File(classloader.getResource("sowpods.txt").getFile());
-      Logger.info(file.toPath().toString());
-      List<String> lines = Files.readAllLines(file.toPath());
-      wordList = new HashSet<String>(lines);
-      Logger.info("wordlist loaded: " + wordList.size());
-    } catch (Exception e) {
-      e.printStackTrace();
-      Logger.error(e.getMessage());
-    }
+      try (InputStream input = getClass().getResourceAsStream("sowpods.txt")) {
+        wordList = new BufferedReader(new InputStreamReader(input)).lines().collect(Collectors.toSet());
+        Logger.info("wordlist loaded: " + wordList.size());
+      } catch (Exception e) {
+        e.printStackTrace();
+        Logger.error(e.getMessage());
+      }
   }
 
   public boolean hasWord(String word) {
